997634692f7c73a39f13dae990e8b9dce40896a8
deogenesj <39394651+deogenesj@users.noreply.github.com>
2021-08-17 00:51:15 -0300

    Create googlenet_n_plot.py

---
+++ googlenet_n_plot.py
@@ -0,0 +1,225 @@
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import AveragePooling2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Dropout
+from keras.layers.core import Dense
+from keras.layers import Flatten
+from keras.layers import Input
+from keras.models import Model
+from keras.layers import concatenate
+import matplotlib
+matplotlib.use("Agg")
+%matplotlib inline
+from sklearn.preprocessing import LabelBinarizer
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import LearningRateScheduler
+from keras.optimizers import SGD
+from keras.datasets import cifar10
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+from tensorflow.keras.utils import to_categorical
+#from sklearn.metrics import plot_confusion_matrix
+
+gpu_info = !nvidia-smi
+gpu_info = '\n'.join(gpu_info)
+if gpu_info.find('failed') >= 0:
+  print('Select the Runtime > "Change runtime type" menu to enable a GPU accelerator, ')
+  print('and then re-execute this cell.')
+else:
+  print(gpu_info)
+
+def conv_module(input,No_of_filters,filtersizeX,filtersizeY,stride,chanDim,padding="same"):
+  input = Conv2D(No_of_filters,(filtersizeX,filtersizeY),strides=stride,padding=padding)(input)
+  input = BatchNormalization(axis=chanDim)(input)
+  input = Activation("relu")(input)
+  return input
+
+
+def inception_module(input,numK1x1,numK3x3,numk5x5,numPoolProj,chanDim):
+                                 #Step 1
+  conv_1x1 = conv_module(input, numK1x1, 1, 1,(1, 1), chanDim) 
+                                 #Step 2
+  conv_3x3 = conv_module(input, numK3x3, 3, 3,(1, 1), chanDim)
+  conv_5x5 = conv_module(input, numk5x5, 5, 5,(1, 1), chanDim)
+                                 #Step 3
+  pool_proj = MaxPooling2D((3, 3), strides=(1, 1), padding='same')(input)
+  pool_proj = Conv2D(numPoolProj, (1, 1), padding='same', activation='relu')(pool_proj)
+                                 #Step 4
+  input = concatenate([conv_1x1, conv_3x3, conv_5x5, pool_proj], axis=chanDim)
+  return input
+
+
+def downsample_module(input,No_of_filters,chanDim):
+  conv_3x3=conv_module(input,No_of_filters,3,3,(2,2),chanDim,padding="valid")
+  pool = MaxPooling2D((3,3),strides=(2,2))(input)
+  input = concatenate([conv_3x3,pool],axis=chanDim)
+  return input
+
+def MiniGoogleNet(width,height,depth,classes):
+  inputShape=(height,width,depth)
+  chanDim=-1
+
+  # (Step 1) Define the model input
+  inputs = Input(shape=inputShape)
+
+  # First CONV module
+  x = conv_module(inputs, 96, 3, 3, (1, 1),chanDim)
+
+  # (Step 2) Two Inception modules followed by a downsample module
+  x = inception_module(x, 32, 32,32,32,chanDim)
+  x = inception_module(x, 32, 48, 48,32,chanDim)
+  x = downsample_module(x, 80, chanDim)
+  
+  # (Step 3) Five Inception modules followed by a downsample module
+  x = inception_module(x, 112, 48, 32, 48,chanDim)
+  x = inception_module(x, 96, 64, 32,32,chanDim)
+  x = inception_module(x, 80, 80, 32,32,chanDim)
+  x = inception_module(x, 48, 96, 32,32,chanDim)
+  x = inception_module(x, 112, 48, 32, 48,chanDim)
+  x = downsample_module(x, 96, chanDim)
+
+  # (Step 4) Two Inception modules followed
+  x = inception_module(x, 176, 160,96,96, chanDim)
+  x = inception_module(x, 176, 160, 96,96,chanDim)
+  
+  # Global POOL and dropout
+  x = AveragePooling2D((7, 7))(x)
+  x = Dropout(0.5)(x)
+
+  # (Step 5) Softmax classifier
+  x = Flatten()(x)
+  x = Dense(classes)(x)
+  x = Activation("softmax")(x)
+
+  # Create the model
+  model = Model(inputs, x, name="googlenet")
+  return model
+
+#Defining function for confusion matrix plot
+def plot_confusion_matrix(ytrue, ypred, classes, normalize=False, title=None, cmap=plt.cm.Blues):
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(ytrue, ypred)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+#Print Confusion matrix
+    fig, ax = plt.subplots(figsize=(7,7))
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
+np.set_printoptions(precision=2)
+
+model = MiniGoogleNet(width=32, height=32, depth=3, classes=10)
+print(model.summary())
+
+NUM_EPOCHS = 10
+INIT_LR = 5e-3
+def poly_decay(epoch):
+  maxEpochs = NUM_EPOCHS
+  baseLR = INIT_LR
+  power = 1.0
+  alpha = baseLR * (1 - (epoch / float(maxEpochs))) ** power
+  return alpha
+
+((trainX, trainY), (testX, testY)) = cifar10.load_data()
+trainX = trainX.astype("float")
+testX = testX.astype("float")
+                                # Step 1
+mean = np.mean(trainX, axis=0)
+trainX -= mean
+testX -= mean
+                                # Step 2
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+                                # Step 3
+aug = ImageDataGenerator(width_shift_range=0.1,height_shift_range=0.1, horizontal_flip=True,fill_mode="nearest")
+
+callbacks=[LearningRateScheduler(poly_decay)]
+opt = SGD(lr=INIT_LR, momentum=0.9)
+model = MiniGoogleNet(width=32, height=32, depth=3, classes=10)
+                                    # Step 1
+model.compile(loss="categorical_crossentropy", optimizer=opt,metrics=["accuracy"])
+                                    # Step 2
+H = model.fit(aug.flow(trainX, trainY, batch_size=64),validation_data=(testX, testY), steps_per_epoch=len(trainX) // 64,epochs=NUM_EPOCHS, callbacks=callbacks, verbose=1)
+
+print("[INFO] serializing network...")
+model.save("googlenet_cnn.h5")
+
+score=model.evaluate(testX,testY)
+print('Test Score=',score[0])
+print('Test Accuracy=',score[1])
+
+#y_pred = model.predict(testX)
+#y_true = np.argmax(testY, axis=1)
+#y_test = to_categorical(testY)
+
+y_test_arg = np.argmax(testY,axis=1)
+y_pred = np.argmax(model.predict(testX),axis=1)
+print('y_test', y_test_arg)
+print('y_pred', y_pred)
+
+plt.style.use("ggplot")
+plt.figure()
+N = NUM_EPOCHS
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Treinando Loss e Accuracy no CIFAR-10")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig("plot")
+
+confusion_mtx= confusion_matrix(y_test_arg, y_pred)
+print('matriz de confusão', confusion_mtx)
+
+class_names=['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+plot_confusion_matrix(y_test_arg, y_pred, classes = class_names, title = 'Matriz de Confusão, sem normalização')
+plot_confusion_matrix(y_test_arg, y_pred, classes = class_names, normalize=True, title='Matrix de Confusão Normalizada')
+
+
+#Classification accuracy
+from sklearn.metrics import accuracy_score
+acc_score = accuracy_score(y_true, y_pred)
+print('Accuracy Score = ', acc_score)
+
+
+
+
